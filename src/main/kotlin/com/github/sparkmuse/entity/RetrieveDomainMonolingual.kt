0d959f55637695bbe0e555f415a74bc19d956a4d
sparkmuse <sparkmuse.tech@gmail.com>
2020-10-18 22:33:08 +0200

    Merged

--- src/main/kotlin/com/github/sparkmuse/entity/RetrieveDomainMonolingual.kt
+++
@@ -1,6 +0,0 @@
-package com.github.sparkmuse.entity
-
-data class RetrieveDomainMonolingual(
-    val metadata: Map<String, String> = mapOf(),
-    val results: Map<String, Map<String, String>> = mapOf()
-)
