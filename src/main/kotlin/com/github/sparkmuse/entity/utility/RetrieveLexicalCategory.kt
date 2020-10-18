30077ac634c886d64f9b00fc441c1d7b4d15e19c
sparkmuse <sparkmuse.tech@gmail.com>
2020-10-19 00:36:45 +0200

    Add support for RetrieveLexicalCategory

--- src/main/kotlin/com/github/sparkmuse/entity/utility/RetrieveLexicalCategory.kt
+++ src/main/kotlin/com/github/sparkmuse/entity/utility/RetrieveLexicalCategory.kt
@@ -2,27 +2,5 @@ package com.github.sparkmuse.entity.utility
 
 data class RetrieveLexicalCategory(
     val metadata: Map<String, String> = mapOf(),
-    val results: Results = Results()
-) {
-    data class Results(
-        val adjective: Map<String, String> = mapOf(),
-        val adverb: Map<String, String> = mapOf(),
-        val combiningForm: Map<String, String> = mapOf(),
-        val conjunction: Map<String, String> = mapOf(),
-        val contraction: Map<String, String> = mapOf(),
-        val determiner: Map<String, String> = mapOf(),
-        val idiomatic: Map<String, String> = mapOf(),
-        val interjection: Map<String, String> = mapOf(),
-        val noun: Map<String, String> = mapOf(),
-        val numeral: Map<String, String> = mapOf(),
-        val other: Map<String, String> = mapOf(),
-        val particle: Map<String, String> = mapOf(),
-        val predeterminer: Map<String, String> = mapOf(),
-        val prefix: Map<String, String> = mapOf(),
-        val preposition: Map<String, String> = mapOf(),
-        val pronoun: Map<String, String> = mapOf(),
-        val residual: Map<String, String> = mapOf(),
-        val suffix: Map<String, String> = mapOf(),
-        val verb: Map<String, String> = mapOf(),
-    )
-}
+    val results: Map<String, Map<String, String>> = mapOf()
+)
